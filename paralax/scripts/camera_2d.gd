031a4de7b51efb3881555a6dceefcaef1accfb11
André Yuhji Terada <andre.terada@ifood.com.br>
2025-01-25 11:11:42 -0300

    1-paralax

---
+++ paralax/scripts/camera_2d.gd
@@ -0,0 +1,12 @@
+extends Camera2D
+
+var SPEED = 500
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var axis_x = Input.get_axis("left", "right")
+	position.x += axis * SPEED * delta
